451751798ec3dfec46ead04c43b76d22a21ae4b9
John Doe <johndoe@example.com>
2021-01-28 15:18:22 +0800

    ok

--- test.py
+++ test.py
@@ -1,9 +1,18 @@
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
+
 img = cv2.imread('hu.jpg',1)
+
+img1 = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+
 print(img)
-cv2.namedWindow('hu', cv2.WINDOW_NORMAL)
-cv2.imshow('hu', img)
-cv2.imwrite('hu2.jpg',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+plt.imshow(img1)
+plt.xticks([]), plt.yticks([])
+plt.show()
+
+# cv2.namedWindow('hu', cv2.WINDOW_NORMAL)
+# cv2.imshow('hu', img)
+# cv2.imwrite('hu2.jpg',img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
